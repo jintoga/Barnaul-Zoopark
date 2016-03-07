3870acefd68fed316bc13f617be8706255f0923c
Dat <jintoga123@yahoo.com>
2016-03-08 03:54:23 +0600

    added startup screen

---
+++ app/src/main/java/com/dat/barnaulzoopark/ui/startup/StartupActivity.java
@@ -0,0 +1,15 @@
+package com.dat.barnaulzoopark.ui.startup;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.dat.barnaulzoopark.R;
+
+public class StartupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startup);
+    }
+}
