dba68dcfadc698eea19f61855893c74511bfd6b9
Dat <jintoga123@yahoo.com>
2016-06-12 01:06:20 +0700

    - toolbar and statusbar look pretty much as i expected, need more workings on "recyclerview not fully displays"

--- app/src/main/java/com/dat/barnaulzoopark/ui/gallery/PhotoAlbumsFragment.java
+++ app/src/main/java/com/dat/barnaulzoopark/ui/gallery/PhotoAlbumsFragment.java
@@ -8,14 +8,17 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import butterknife.Bind;
-import butterknife.ButterKnife;
+
 import com.dat.barnaulzoopark.R;
 import com.dat.barnaulzoopark.ui.DummyGenerator;
 import com.dat.barnaulzoopark.ui.TempBaseFragment;
 import com.dat.barnaulzoopark.ui.gallery.gallerydetails.GridSpacingItemDecoration;
 import com.dat.barnaulzoopark.ui.gallery.model.PhotoAlbum;
+
 import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Created by DAT on 07-Feb-16.
@@ -25,23 +28,16 @@ public class PhotoAlbumsFragment extends TempBaseFragment {
     @Bind(R.id.photoAlbums)
     protected RecyclerView photoAlbums;
     private PhotoAlbumsAdapter adapter;
-    @Bind(R.id.systemBar)
-    protected View systemBar;
     View view;
     private GridLayoutManager gridlayoutManager;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
-        @Nullable Bundle savedInstanceState) {
+                             @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_gallery, container, false);
         ButterKnife.bind(this, view);
-        if (systemBar != null) {
-            systemBar.getLayoutParams().height = getStatusBarHeight();
-            systemBar.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
-            systemBar.bringToFront();
-            systemBar.requestLayout();
-        }
+
         initRecyclerView();
         return view;
     }
@@ -50,14 +46,14 @@ public class PhotoAlbumsFragment extends TempBaseFragment {
         if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
             gridlayoutManager = new GridLayoutManager(getContext(), 3);
             photoAlbums.addItemDecoration(new GridSpacingItemDecoration(3,
-                getContext().getResources().getDimensionPixelSize(R.dimen.photo_album_items_span),
-                true));
+                    getContext().getResources().getDimensionPixelSize(R.dimen.photo_album_items_span),
+                    true));
         } else {
 
             gridlayoutManager = new GridLayoutManager(getContext(), 2);
             photoAlbums.addItemDecoration(new GridSpacingItemDecoration(2,
-                getContext().getResources().getDimensionPixelSize(R.dimen.photo_album_items_span),
-                true));
+                    getContext().getResources().getDimensionPixelSize(R.dimen.photo_album_items_span),
+                    true));
         }
 
         photoAlbums.setHasFixedSize(true);
