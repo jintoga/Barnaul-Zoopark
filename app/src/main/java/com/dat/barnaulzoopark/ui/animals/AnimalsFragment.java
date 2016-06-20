26227b9d2013fb666eeb93ffe4eb5cd5cbbaac0c
Nguyen Quoc Dat <jintoga123@yahoo.com>
2016-06-20 17:32:50 +0700

    - added temp methods for getting animals list - fixed banner's height

--- app/src/main/java/com/dat/barnaulzoopark/ui/animals/AnimalsFragment.java
+++ app/src/main/java/com/dat/barnaulzoopark/ui/animals/AnimalsFragment.java
@@ -1,8 +1,10 @@
 package com.dat.barnaulzoopark.ui.animals;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -11,20 +13,25 @@ import android.view.ViewGroup;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import com.dat.barnaulzoopark.R;
+import com.dat.barnaulzoopark.ui.DummyGenerator;
 import com.dat.barnaulzoopark.ui.TempBaseFragment;
+import com.dat.barnaulzoopark.ui.gallery.gallerydetails.GridSpacingItemDecoration;
+import com.dat.barnaulzoopark.ui.gallery.model.Photo;
 import com.dat.barnaulzoopark.widget.SearchView.FloatingSearchView;
 
 /**
  * Created by Nguyen on 6/17/2016.
  */
 public class AnimalsFragment extends TempBaseFragment
-    implements FloatingSearchView.SearchViewFocusedListener {
+    implements FloatingSearchView.SearchViewFocusedListener, AnimalsAdapter.AnimalsAdapterListener {
     @Bind(R.id.systemBar)
     protected View systemBar;
     @Bind(R.id.search_view)
     protected FloatingSearchView searchView;
     @Bind(R.id.animals)
     protected RecyclerView animals;
+    private AnimalsAdapter animalsAdapter;
+    private GridLayoutManager layoutManager;
     private View view;
 
     @Nullable
@@ -41,9 +48,40 @@ public class AnimalsFragment extends TempBaseFragment
         return view;
     }
 
+    @Override
+    public void onPhotoSelected(@NonNull Photo photo) {
+
+    }
+
     private void init() {
         searchView.setSearchViewFocusedListener(this);
-        animals.setLayoutManager(new LinearLayoutManager(getContext()));
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            layoutManager = new GridLayoutManager(getContext(), 3);
+            animals.addItemDecoration(new GridSpacingItemDecoration(3,
+                getContext().getResources().getDimensionPixelSize(R.dimen.photo_gallery_items_span),
+                true));
+        } else {
+            layoutManager = new GridLayoutManager(getContext(), 2);
+            animals.addItemDecoration(new GridSpacingItemDecoration(2,
+                getContext().getResources().getDimensionPixelSize(R.dimen.photo_gallery_items_span),
+                true));
+        }
+        animals.setLayoutManager(layoutManager);
+        if (animalsAdapter == null) {
+            animalsAdapter = new AnimalsAdapter(this);
+        }
+        animals.setAdapter(animalsAdapter);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        loadData();
+    }
+
+    public void loadData() {
+        animalsAdapter.setData(DummyGenerator.getAnimalsPhotos());
+        animalsAdapter.notifyDataSetChanged();
     }
 
     @Override
