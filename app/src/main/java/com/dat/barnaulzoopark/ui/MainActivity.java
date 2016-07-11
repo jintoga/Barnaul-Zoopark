9ac5167c7ef54d245a36fad283b080c1b50720d8
Nguyen Quoc Dat <jintoga123@yahoo.com>
2016-07-11 16:11:09 +0700

    - changed animatedHeaderImage to true - changed SearchView's background onTouch to onClick - added DrawerListener in MainActivity to help close SearchView when NavDrawer is open

--- app/src/main/java/com/dat/barnaulzoopark/ui/MainActivity.java
+++ app/src/main/java/com/dat/barnaulzoopark/ui/MainActivity.java
@@ -9,19 +9,45 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import com.dat.barnaulzoopark.R;
 import com.dat.barnaulzoopark.ui.animals.AnimalsFragment;
 import com.dat.barnaulzoopark.ui.gallery.PhotoAlbumsFragment;
 
-public class MainActivity extends AppCompatActivity implements OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity
+    implements OnNavigationItemSelectedListener, DrawerLayout.DrawerListener {
 
     @Bind(R.id.navigation_view)
     protected NavigationView navigationView;
     @Bind(R.id.drawer)
     protected DrawerLayout drawerLayout;
     private int currentMenuItemID = -1;
+    DrawerListener drawerListener;
+
+    @Override
+    public void onDrawerSlide(View drawerView, float slideOffset) {
+        //Ignore
+    }
+
+    @Override
+    public void onDrawerOpened(View drawerView) {
+        if (drawerListener != null) {
+            //drawer is open, SearchView should be closed
+            drawerListener.onDrawerOpen();
+        }
+    }
+
+    @Override
+    public void onDrawerClosed(View drawerView) {
+        //Ignore
+    }
+
+    @Override
+    public void onDrawerStateChanged(int newState) {
+        //Ignore
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +64,7 @@ public class MainActivity extends AppCompatActivity implements OnNavigationItemS
 
     private void setupNavDrawer() {
         navigationView.setNavigationItemSelectedListener(this);
+        drawerLayout.addDrawerListener(this);
     }
 
     @Override
@@ -74,5 +101,13 @@ public class MainActivity extends AppCompatActivity implements OnNavigationItemS
 
     public void closeDrawer() {
         drawerLayout.closeDrawer(GravityCompat.START);
+    }
+
+    public interface DrawerListener {
+        void onDrawerOpen();
+    }
+
+    public void setDrawerListener(DrawerListener drawerListener) {
+        this.drawerListener = drawerListener;
     }
 }
