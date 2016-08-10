933c195a1ec3e4858a95e4e96a52eb237ce5723e
unknown <jintoga123@yahoo.com>
2016-08-10 19:59:20 +0700

    - added PhotoView for Fresco lib - added PhotosDetailActivity

---
+++ app/src/main/java/com/dat/barnaulzoopark/ui/photosdetail/PhotosDetailActivity.java
@@ -0,0 +1,14 @@
+package com.dat.barnaulzoopark.ui.photosdetail;
+
+import android.os.Bundle;
+import com.dat.barnaulzoopark.R;
+import com.dat.barnaulzoopark.ui.BaseActivity;
+
+public class PhotosDetailActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photos_detail);
+    }
+}
