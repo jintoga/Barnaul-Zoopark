86d1db948f029a92f79142c4283f88f6b78a1b87
Dat <jintoga123@yahoo.com>
2017-01-05 19:56:28 +0700

    enabled Firebase offline capabilities

--- app/src/main/java/com/dat/barnaulzoopark/BZApplication.java
+++ app/src/main/java/com/dat/barnaulzoopark/BZApplication.java
@@ -1,9 +1,9 @@
 package com.dat.barnaulzoopark;
 
 import android.app.Application;
-
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.github.florent37.materialviewpager.MaterialViewPagerAnimator;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * Created by DAT on 03-May-16.
@@ -12,6 +12,7 @@ public class BZApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true); //FireBase offline capabilities
         Fresco.initialize(this);
         MaterialViewPagerAnimator.ENABLE_LOG = false;
     }
