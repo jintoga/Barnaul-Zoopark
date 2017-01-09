2492e8cdaf48d235c2bc665c40dd98fec27ef0c5
Dat <jintoga123@yahoo.com>
2017-01-10 01:45:26 +0700

    working on layout

--- app/src/main/java/com/dat/barnaulzoopark/ui/BaseActivity.java
+++ app/src/main/java/com/dat/barnaulzoopark/ui/BaseActivity.java
@@ -1,5 +1,6 @@
 package com.dat.barnaulzoopark.ui;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -20,5 +21,14 @@ public class BaseActivity extends AppCompatActivity {
         if (withAnimation) {
             overridePendingTransition(R.anim.pull_in_left, R.anim.push_out_right);
         }
+    }
+
+    protected int getStatusBarHeight() {
+        int result = 0;
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0 && Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            result = getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
     }
 }
--- app/src/main/java/com/dat/barnaulzoopark/ui/newsdetail/NewsDetailActivity.java
+++ app/src/main/java/com/dat/barnaulzoopark/ui/newsdetail/NewsDetailActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import butterknife.Bind;
@@ -38,6 +39,10 @@ public class NewsDetailActivity extends BaseActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setHomeButtonEnabled(true);
         }
+        CollapsingToolbarLayout.LayoutParams layoutParams =
+            (CollapsingToolbarLayout.LayoutParams) toolbar.getLayoutParams();
+        layoutParams.topMargin = getStatusBarHeight();
+        toolbar.setLayoutParams(layoutParams);
     }
 
     @Override
