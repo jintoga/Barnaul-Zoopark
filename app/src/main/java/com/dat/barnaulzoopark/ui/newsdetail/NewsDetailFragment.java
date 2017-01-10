59cc9eaa6720823ec3f8d5f003a509454c959bb0
Dat <jintoga123@yahoo.com>
2017-01-11 01:01:07 +0700

    fixed

--- app/src/main/java/com/dat/barnaulzoopark/ui/newsdetail/NewsDetailFragment.java
+++ app/src/main/java/com/dat/barnaulzoopark/ui/newsdetail/NewsDetailFragment.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import butterknife.Bind;
 import butterknife.ButterKnife;
 import com.dat.barnaulzoopark.R;
 import com.dat.barnaulzoopark.ui.BaseMvpFragment;
@@ -17,6 +18,9 @@ import com.dat.barnaulzoopark.ui.BaseMvpFragment;
 public class NewsDetailFragment
     extends BaseMvpFragment<NewsDetailContract.View, NewsDetailContract.UserActionListener>
     implements NewsDetailContract.View {
+
+    @Bind(R.id.content_container)
+    protected View contentContainer;
 
     @NonNull
     @Override
@@ -30,6 +34,12 @@ public class NewsDetailFragment
         @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_news_detail, container, false);
         ButterKnife.bind(this, view);
+        contentContainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int viewId = view.getId();
+            }
+        });
         return view;
     }
 
