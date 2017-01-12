c2d0c69b7a7a27eb25f565302ec90b963f2c60e4
Nguyen Quoc Dat <d.nguyen@freematiq.com>
2017-01-12 10:01:32 +0700

    moved NewsDetail header to NewsDetailFragment, NewsDetailActivity only contains fragment

--- app/src/main/java/com/dat/barnaulzoopark/ui/newsdetail/NewsDetailActivity.java
+++ app/src/main/java/com/dat/barnaulzoopark/ui/newsdetail/NewsDetailActivity.java
@@ -5,10 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.CollapsingToolbarLayout;
-import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import butterknife.Bind;
 import butterknife.ButterKnife;
 import com.dat.barnaulzoopark.R;
 import com.dat.barnaulzoopark.ui.BaseActivity;
@@ -18,9 +15,6 @@ import com.dat.barnaulzoopark.ui.BaseActivity;
  */
 
 public class NewsDetailActivity extends BaseActivity {
-
-    @Bind(R.id.toolbar)
-    protected Toolbar toolbar;
 
     public static void startActivity(Activity activity, @NonNull String uid) {
         Intent intent = new Intent(activity, NewsDetailActivity.class);
@@ -33,16 +27,6 @@ public class NewsDetailActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_detail);
         ButterKnife.bind(this);
-        setSupportActionBar(toolbar);
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setTitle("");
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-            getSupportActionBar().setHomeButtonEnabled(true);
-        }
-        CollapsingToolbarLayout.LayoutParams layoutParams =
-            (CollapsingToolbarLayout.LayoutParams) toolbar.getLayoutParams();
-        layoutParams.topMargin = getStatusBarHeight();
-        toolbar.setLayoutParams(layoutParams);
     }
 
     @Override
--- app/src/main/java/com/dat/barnaulzoopark/ui/newsdetail/NewsDetailFragment.java
+++ app/src/main/java/com/dat/barnaulzoopark/ui/newsdetail/NewsDetailFragment.java
@@ -3,6 +3,9 @@ package com.dat.barnaulzoopark.ui.newsdetail;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +22,8 @@ public class NewsDetailFragment
     extends BaseMvpFragment<NewsDetailContract.View, NewsDetailContract.UserActionListener>
     implements NewsDetailContract.View {
 
+    @Bind(R.id.toolbar)
+    protected Toolbar toolbar;
     @Bind(R.id.content_container)
     protected View contentContainer;
 
@@ -34,6 +39,17 @@ public class NewsDetailFragment
         @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_news_detail, container, false);
         ButterKnife.bind(this, view);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        if (((AppCompatActivity) getActivity()).getSupportActionBar() != null) {
+            ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("");
+            ((AppCompatActivity) getActivity()).getSupportActionBar()
+                .setDisplayHomeAsUpEnabled(true);
+            ((AppCompatActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        }
+        CollapsingToolbarLayout.LayoutParams layoutParams =
+            (CollapsingToolbarLayout.LayoutParams) toolbar.getLayoutParams();
+        layoutParams.topMargin = getStatusBarHeight();
+        toolbar.setLayoutParams(layoutParams);
         contentContainer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
