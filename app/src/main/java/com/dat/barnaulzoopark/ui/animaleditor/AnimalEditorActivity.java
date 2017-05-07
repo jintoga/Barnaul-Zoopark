52607dc11a1bfd5e140d143d0699458811817267
Nguyen Quoc Dat <jintoga123@yahoo.com>
2017-05-07 20:11:35 +0700

    fixed

--- app/src/main/java/com/dat/barnaulzoopark/ui/animaleditor/AnimalEditorActivity.java
+++ app/src/main/java/com/dat/barnaulzoopark/ui/animaleditor/AnimalEditorActivity.java
@@ -6,22 +6,30 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import com.afollestad.materialdialogs.MaterialDialog;
+import com.bumptech.glide.Glide;
 import com.dat.barnaulzoopark.BZApplication;
 import com.dat.barnaulzoopark.R;
 import com.dat.barnaulzoopark.adapters.MultiFileAttachmentAdapter;
+import com.dat.barnaulzoopark.model.Attachment;
 import com.dat.barnaulzoopark.model.animal.Animal;
 import com.dat.barnaulzoopark.model.animal.Species;
 import com.dat.barnaulzoopark.ui.BZDialogBuilder;
@@ -43,6 +51,10 @@ public class AnimalEditorActivity extends
 
     private static final String EXTRA_SELECTED_ANIMAL_UID = "EXTRA_SELECTED_ANIMAL_UID";
     private static final String TAG = AnimalEditorActivity.class.getName();
+    private static final int REQUEST_BROWSE_IMAGE_BANNER = 1;
+    private static final int REQUEST_BROWSE_IMAGE_ICON = 3;
+    private static final int REQUEST_BROWSE_IMAGE_ATTACHMENT = 2;
+
     @Bind(R.id.toolbar)
     protected Toolbar toolbar;
 
@@ -50,6 +62,10 @@ public class AnimalEditorActivity extends
     protected ImageView bannerImage;
     @Bind(R.id.icon)
     protected ImageView icon;
+    @Bind(R.id.remove)
+    protected ImageButton remove;
+    @Bind(R.id.attach)
+    protected ImageButton attach;
     @Bind(R.id.name)
     protected EditText name;
     @Bind(R.id.species)
@@ -137,6 +153,17 @@ public class AnimalEditorActivity extends
         album.addItemDecoration(new MultiAttachmentDecoration(
             (int) getResources().getDimension(R.dimen.item_file_attachment_decoration)));
         attachmentAdapter = new MultiFileAttachmentAdapter(this);
+        album.setAdapter(attachmentAdapter);
+        String selectedAnimalUid = getIntent().getStringExtra(EXTRA_SELECTED_ANIMAL_UID);
+        if (selectedAnimalUid != null) {
+            //ToDo: implement edit
+            //loadSelectedAnimal(selectedAnimalUid);
+            //attachmentAdapter.setEditingMode(true);
+        } else {
+            attachmentAdapter.addEmptySlot();
+        }
+
+        video.setPrefix("https://www.youtube.com/watch?v=");
     }
 
     private void initSpinners() {
@@ -171,27 +198,101 @@ public class AnimalEditorActivity extends
 
     @Override
     public void onRemoved(int position) {
-
+        album.requestFocus();
+        if (filledAttachmentCounter == MultiFileAttachmentAdapter.MAX_NUMBER_ATTACHMENT) {
+            attachmentAdapter.emptySlot(position);
+            attachmentAdapter.addEmptySlot();
+        } else {
+            attachmentAdapter.emptySlot(position);
+        }
+        filledAttachmentCounter--;
     }
 
     @Override
     public void onSlotSelected(int position) {
-
+        album.requestFocus();
+        if (filledAttachmentCounter == MultiFileAttachmentAdapter.MAX_NUMBER_ATTACHMENT) {
+            Toast.makeText(this, "Выбрано максимальное количество файлов", Toast.LENGTH_LONG)
+                .show();
+            return;
+        }
+        currentAttachmentPosition = position;
+        createChangePhotoDialog(REQUEST_BROWSE_IMAGE_ATTACHMENT, false);
     }
 
     @Override
     public void onRemovedPhotoClicked() {
-
+        Log.d(TAG, "onRemovedPhotoClicked");
+        bannerImageUri = null;
+        bannerImage.setImageDrawable(
+            ContextCompat.getDrawable(this, R.drawable.img_photo_gallery_placeholder));
     }
 
     @Override
     public void onResultUriSuccess(@NonNull Uri uri, int originalRequestCode) {
-
+        switch (originalRequestCode) {
+            case REQUEST_BROWSE_IMAGE_BANNER:
+                Log.d(TAG, "REQUEST_BROWSE_IMAGE_BANNER");
+                bannerImageUri = uri;
+                Glide.with(this).load(uri).into(bannerImage);
+                break;
+            case REQUEST_BROWSE_IMAGE_ICON:
+                Log.d(TAG, "REQUEST_BROWSE_IMAGE_ICON");
+                iconUri = uri;
+                Glide.with(this).load(uri).into(icon);
+                updateAttachIconButtons(true);
+                break;
+            case REQUEST_BROWSE_IMAGE_ATTACHMENT:
+                Log.d(TAG, "REQUEST_BROWSE_IMAGE_ATTACHMENT");
+                filledAttachmentCounter++;
+                Attachment attachment = new Attachment(true, uri.toString());
+                attachmentAdapter.fillSlot(currentAttachmentPosition, attachment);
+                attachmentAdapter.addEmptySlot();
+                album.smoothScrollToPosition(attachmentAdapter.getItemCount() - 1);
+                break;
+            default:
+                break;
+        }
     }
 
     @Override
     public void onCropError(@NonNull String errorMsg) {
+        Log.d(TAG, errorMsg);
+        showSnackBar(errorMsg);
+    }
 
+    @OnClick(R.id.bannerImageContainer)
+    protected void bannerImageContainerClicked() {
+        createChangePhotoDialog(REQUEST_BROWSE_IMAGE_BANNER, true);
+    }
+
+    @OnClick(R.id.attach)
+    protected void attachIconClicked() {
+        createChangePhotoDialog(REQUEST_BROWSE_IMAGE_ICON, false);
+    }
+
+    @OnClick(R.id.remove)
+    protected void removeIconClicked() {
+        this.iconUri = null;
+        this.icon.setImageDrawable(null);
+        updateAttachIconButtons(false);
+    }
+
+    private void updateAttachIconButtons(boolean isFilled) {
+        if (isFilled) {
+            icon.setVisibility(View.VISIBLE);
+            attach.setVisibility(View.GONE);
+            remove.setVisibility(View.VISIBLE);
+        } else {
+            icon.setVisibility(View.GONE);
+            attach.setVisibility(View.VISIBLE);
+            remove.setVisibility(View.GONE);
+        }
+    }
+
+    private void showSnackBar(@NonNull String msg) {
+        Snackbar snackbar = Snackbar.make(findViewById(R.id.container), msg, Snackbar.LENGTH_SHORT);
+        snackbar.show();
     }
 
     @Override
