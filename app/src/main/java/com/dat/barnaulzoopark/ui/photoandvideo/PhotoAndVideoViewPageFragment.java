02979e6c91faf5f4e38a3efd2b763c328bc56410
Nguyen Quoc Dat <jintoga123@yahoo.com>
2017-05-23 04:22:16 +0700

    f

--- app/src/main/java/com/dat/barnaulzoopark/ui/photoandvideo/PhotoAndVideoViewPageFragment.java
+++ app/src/main/java/com/dat/barnaulzoopark/ui/photoandvideo/PhotoAndVideoViewPageFragment.java
@@ -13,16 +13,14 @@ import butterknife.ButterKnife;
 import com.dat.barnaulzoopark.BZApplication;
 import com.dat.barnaulzoopark.R;
 import com.dat.barnaulzoopark.api.BZFireBaseApi;
-import com.dat.barnaulzoopark.model.DummyGenerator;
 import com.dat.barnaulzoopark.model.PhotoAlbum;
-import com.dat.barnaulzoopark.model.TempPhotoAlbum;
+import com.dat.barnaulzoopark.model.VideoAlbum;
 import com.dat.barnaulzoopark.ui.BaseFragment;
 import com.dat.barnaulzoopark.ui.photoandvideo.adapters.PhotoAlbumsAdapter;
 import com.dat.barnaulzoopark.ui.photoandvideo.adapters.VideoAlbumsAdapter;
 import com.dat.barnaulzoopark.ui.recyclerviewdecorations.GridSpacingItemDecoration;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import java.util.List;
 
 /**
  * Created by Nguyen on 7/13/2016.
@@ -31,8 +29,6 @@ public class PhotoAndVideoViewPageFragment extends BaseFragment {
 
     @Bind(R.id.photoAlbums)
     protected RecyclerView multiMediaList;
-    private PhotoAlbumsAdapter photoAlbumsAdapter;
-    private VideoAlbumsAdapter videoAlbumsAdapter;
 
     private static final String KEY_FRAGMENT_TYPE = "FRAGMENT_TYPE";
     public static final int PHOTO_TYPE = 1;
@@ -76,20 +72,19 @@ public class PhotoAndVideoViewPageFragment extends BaseFragment {
         multiMediaList.setLayoutManager(gridlayoutManager);
         FirebaseDatabase database =
             BZApplication.get(getContext()).getApplicationComponent().fireBaseDatabase();
+        DatabaseReference databaseReference;
         if (type == PHOTO_TYPE) {
-            DatabaseReference databaseReference =
-                database.getReference().child(BZFireBaseApi.photo_album);
-            photoAlbumsAdapter =
+            databaseReference = database.getReference().child(BZFireBaseApi.photo_album);
+            PhotoAlbumsAdapter photoAlbumsAdapter =
                 new PhotoAlbumsAdapter(PhotoAlbum.class, R.layout.item_photo_albums,
                     PhotoAlbumsAdapter.ViewHolder.class, databaseReference, getActivity());
             multiMediaList.setAdapter(photoAlbumsAdapter);
         } else if (type == VIDEO_TYPE) {
-            videoAlbumsAdapter = new VideoAlbumsAdapter(null, getContext());
+            databaseReference = database.getReference().child(BZFireBaseApi.video_album);
+            VideoAlbumsAdapter videoAlbumsAdapter =
+                new VideoAlbumsAdapter(VideoAlbum.class, R.layout.item_video_albums,
+                    VideoAlbumsAdapter.ViewHolder.class, databaseReference, getActivity());
             multiMediaList.setAdapter(videoAlbumsAdapter);
-        }
-        List<TempPhotoAlbum> data = DummyGenerator.getDummyData();
-        if (videoAlbumsAdapter != null) {
-            videoAlbumsAdapter.setData(data);
         }
     }
 }
--- app/src/main/java/com/dat/barnaulzoopark/ui/photoandvideo/adapters/VideoAlbumsAdapter.java
+++ app/src/main/java/com/dat/barnaulzoopark/ui/photoandvideo/adapters/VideoAlbumsAdapter.java
@@ -1,77 +1,69 @@
 package com.dat.barnaulzoopark.ui.photoandvideo.adapters;
 
-import android.content.Context;
+import android.app.Activity;
 import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.TextView;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import com.dat.barnaulzoopark.R;
-import com.dat.barnaulzoopark.model.TempPhotoAlbum;
+import com.dat.barnaulzoopark.model.VideoAlbum;
+import com.dat.barnaulzoopark.utils.ConverterUtils;
 import com.facebook.drawee.view.SimpleDraweeView;
-import java.util.ArrayList;
-import java.util.List;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.Query;
 
 /**
  * Created by Nguyen on 7/15/2016.
  */
-public class VideoAlbumsAdapter extends RecyclerView.Adapter<VideoAlbumsAdapter.ViewHolder> {
+public class VideoAlbumsAdapter
+    extends FirebaseRecyclerAdapter<VideoAlbum, VideoAlbumsAdapter.ViewHolder> {
 
-    private List<TempPhotoAlbum> data;
-    private Context context;
+    private Activity activity;
 
-    public VideoAlbumsAdapter(List<TempPhotoAlbum> data, Context context) {
-        this.data = data;
-        this.context = context;
+    /**
+     * @param modelClass Firebase will marshall the data at a location into an instance of a class
+     * that
+     * you provide
+     * @param modelLayout This is the layout used to represent a single item in the list. You will
+     * be
+     * responsible for populating an
+     * instance of the corresponding view with the data from an instance of modelClass.
+     * @param viewHolderClass The class that hold references to all sub-views in an instance
+     * modelLayout.
+     * @param ref The Firebase location to watch for data changes. Can also be a slice of a
+     * location,
+     * using some
+     * combination of {@code limit()}, {@code startAt()}, and {@code endAt()}.
+     */
+    public VideoAlbumsAdapter(Class<VideoAlbum> modelClass, int modelLayout,
+        Class<ViewHolder> viewHolderClass, Query ref, Activity activity) {
+        super(modelClass, modelLayout, viewHolderClass, ref);
+        this.activity = activity;
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext())
-            .inflate(R.layout.item_video_albums, parent, false);
-
-        ViewHolder viewHolder = new ViewHolder(view);
-        return viewHolder;
-    }
-
-    public void setData(List<TempPhotoAlbum> data) {
-        if (this.data == null) {
-            this.data = new ArrayList<>();
+    protected void populateViewHolder(ViewHolder viewHolder, VideoAlbum videoAlbum, int position) {
+        if (videoAlbum != null) {
+            Uri imgThumbnail = Uri.parse(videoAlbum.getVideos().values().iterator().next());
+            viewHolder.thumbnail.setImageURI(imgThumbnail);
+            String name = String.format("%s\n%s", videoAlbum.getName(),
+                ConverterUtils.getConvertedTime(videoAlbum.getTime()));
+            viewHolder.albumName.setText(name);
+            viewHolder.count.setText(String.valueOf(videoAlbum.getVideos().size()));
         }
-        this.data.clear();
-        this.data.addAll(data);
-        notifyDataSetChanged();
+        /*viewHolder.itemView.setOnClickListener(
+            v -> PhotoAlbumsDetailActivity.startActivity(activity, photoAlbum));*/
     }
 
-    @Override
-    public void onBindViewHolder(final ViewHolder holder, int position) {
-        if (data != null && data.size() > 0) {
-            final TempPhotoAlbum photoAlbum = data.get(position);
-            if (photoAlbum != null) {
-                Uri imgThumbnail = Uri.parse(photoAlbum.getUrls()[0]);
-                holder.thumbnail.setImageURI(imgThumbnail);
-            }
-            holder.itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                }
-            });
-        }
-    }
-
-    @Override
-    public int getItemCount() {
-        if (data != null) {
-            return data.size();
-        }
-        return 0;
-    }
-
-    static class ViewHolder extends RecyclerView.ViewHolder {
+    public static class ViewHolder extends RecyclerView.ViewHolder {
         @Bind(R.id.thumbnail)
         protected SimpleDraweeView thumbnail;
+        @Bind(R.id.albumName)
+        TextView albumName;
+        @Bind(R.id.count)
+        TextView count;
 
         public ViewHolder(View itemView) {
             super(itemView);
